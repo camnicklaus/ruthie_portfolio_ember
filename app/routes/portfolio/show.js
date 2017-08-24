37e79ee6c42b809c0d0e66d4413ba9b9a0f05c87
Cameron Nicklaus <camnicklaus@gmail.com>
2017-08-23 22:44:07 -0700

    image gallery

--- app/routes/portfolio/show.js
+++ app/routes/portfolio/show.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
-
-
 export default Ember.Route.extend({
   model(params) {
-    console.log(params  )
+    let images = this.modelFor('portfolio');
+    let image = images.findBy('id', params.image_id);
+    console.log(params)
     // debugger;
-    return params;
+    return image;
  
   }
 });
